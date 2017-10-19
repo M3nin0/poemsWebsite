570507065127a9db0ed2b349cac13f287f0d532b
Felipe Menino <efelipecarlos@gmail.com>
2017-10-19 20:52:57 -0200

    Add update do banco de dados

--- js/db.js
+++ js/db.js
@@ -1,3 +1,29 @@
 function connect(){
-  var db = openDatabase('poems', '1.0', 'poemsdb', 2 * 1024 * 1024);
+  db = openDatabase('poems', '1.0', 'poemsdb', 2 * 1024 * 1024);
+}
+
+// Cria tabelas para os poemas
+function createTable() {
+  db.transaction(function(tx){
+    tx.executeSql('CREATE TABLE IF NOT EXISTS poemas (ID INTEGER PRIMARY KEY, poem TEXT )');
+  });
+}
+
+connect();
+createTable();
+
+function salva(){
+
+  var element = 2;
+  var texto = document.getElementById(element).innerHTML;
+
+  while(texto != null){
+
+    db.transaction(function(tx){
+      tx.executeSql('INSERT INTO poemas (id, poem) VALUES(?, ?)', [element, texto]);
+    });
+
+    element += 1;
+    texto = document.getElementById(element).innerHTML;
+  }
 }
