00f44456d694981572986b520045760fdc703484
Felipe Menino <efelipecarlos@gmail.com>
2017-10-19 10:22:47 -0200

    Remoção de botão e adição de input

--- js/utils.js
+++ js/utils.js
@@ -2,17 +2,18 @@ var cont = 3;
 
 function addPoem(){
 
+  var texto = prompt("Insira o seu poema");
+
   var newPoem = document.createElement('div');
   newPoem.className = "container-fluid espace";
   newPoem.id = cont;
   document.getElementsByTagName('section')[0].appendChild(newPoem);
 
   var doc = document.getElementById(cont);
-  doc.innerHTML = 'Um poema legal';
+  doc.innerHTML = texto;
 
   cont += 1;
 }
-
 
 function goGithub(){
   location.href = "http://github.com/M3nin0/poemsWebsite";
