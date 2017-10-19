0994a03eb7f6eef129544ec369a0090fa9c0fc77
Felipe Menino <efelipecarlos@gmail.com>
2017-10-19 21:20:27 -0200

    Término da integração com banco de dados cache

--- js/db.js
+++ js/db.js
@@ -1,3 +1,17 @@
+var cont = 3;
+
+function addPoemArgs(text){
+  var newPoem = document.createElement('div');
+  newPoem.className = "container-fluid espace";
+  newPoem.id = cont;
+  document.getElementsByTagName('section')[0].appendChild(newPoem);
+
+  var doc = document.getElementById(cont);
+  doc.innerHTML = text;
+
+  cont += 1;
+}
+
 function connect(){
   db = openDatabase('poems', '1.0', 'poemsdb', 2 * 1024 * 1024);
 }
@@ -16,14 +30,34 @@ function salva(){
 
   var element = 2;
   var texto = document.getElementById(element).innerHTML;
+  var sentinel = document.getElementById(element);
 
-  while(texto != null){
+  while(sentinel != null){
 
     db.transaction(function(tx){
       tx.executeSql('INSERT INTO poemas (id, poem) VALUES(?, ?)', [element, texto]);
     });
 
     element += 1;
-    texto = document.getElementById(element).innerHTML;
+
+    if(document.getElementById(element)!= null){
+      texto = document.getElementById(element).innerHTML;
+    }
+
+    sentinel = document.getElementById(element);
   }
+
+  recovery();
+}
+
+function recovery(){
+
+  db.transaction(function(tx){
+    tx.executeSql('SELECT * FROM poemas', [], function(rx, resultado){
+      var rows = resultado.rows;
+      for(var i = 0; i < rows.length; i++){
+        addPoemArgs(rows[i].poem);
+      }
+    });
+  }, null);
 }
